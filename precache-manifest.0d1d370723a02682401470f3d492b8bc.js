cc2af833cdea29b049d1e01a3f1ed9f923654b58
ProfessionalJK <jawadkazi06@gmail.com>
2020-08-24 18:25:45 +0530

    Updates

---
+++ precache-manifest.0d1d370723a02682401470f3d492b8bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ce209e844fa47414268b03ce709c58",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "1c9c5cecd918dee153c4",
+    "url": "/react-tutorial/static/css/main.f3900a30.chunk.css"
+  },
+  {
+    "revision": "88e9a53498a1a4c7dd68",
+    "url": "/react-tutorial/static/js/2.c3a0d397.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.c3a0d397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c9c5cecd918dee153c4",
+    "url": "/react-tutorial/static/js/main.dd2a6a5e.chunk.js"
+  },
+  {
+    "revision": "c7fdda2cda297de6ddec",
+    "url": "/react-tutorial/static/js/runtime-main.2cd4adde.js"
+  }
+]);
